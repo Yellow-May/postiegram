aad9f5d283ee05ae565e2ec08ab2c3342cebeede
yellow-may <yellow.may.21@gmail.com>
2022-07-26 16:26:54 +0100

    Created useDimensions, to handle responsive designs

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,5 +1,6 @@
 import useRefreshToken from './useRefreshToken';
 import usePrivateAxios from './usePrivateAxios';
 import useQuery from './useQuery';
+import useDimensions from './useDimensions';
 
-export { useRefreshToken, usePrivateAxios, useQuery };
+export { useRefreshToken, usePrivateAxios, useQuery, useDimensions };
