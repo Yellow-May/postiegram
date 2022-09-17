5e6be31ac9460647bc3a800983e34df59faa2a46
yellow-may <yellow.may.21@gmail.com>
2022-09-17 05:43:06 +0100

    code formatting

--- client/src/pages/Home/Sider/index.tsx
+++ client/src/pages/Home/Sider/index.tsx
@@ -1,5 +1,14 @@
 import { FC, useEffect, useState } from 'react';
-import { Avatar, Layout, Space, Image, Typography, List, Divider, Button } from 'antd';
+import {
+	Avatar,
+	Layout,
+	Space,
+	Image,
+	Typography,
+	List,
+	Divider,
+	Button,
+} from 'antd';
 import { useSelector } from 'react-redux';
 import { getUser } from 'redux/features/Auth';
 import { Link } from 'react-router-dom';
@@ -43,20 +52,30 @@ const Sider: FC<SiderProps> = ({ fetchData }) => {
 	}, []);
 
 	const handleFollowRequest = async (user_id: string) => {
-		await axiosPrivate.post('/user/follow', { user_id }, { cancelToken: source.token });
+		await axiosPrivate.post(
+			'/user/follow',
+			{ user_id },
+			{ cancelToken: source.token }
+		);
 		await fetchBots();
 		await fetchData();
 	};
 
 	return (
-		<Layout.Sider style={{ backgroundColor: '#e3e3e3', padding: '24px 0 0 24px' }}>
+		<Layout.Sider
+			style={{ backgroundColor: '#e3e3e3', padding: '24px 0 0 24px' }}>
 			<div style={{ marginBottom: 16 }}>
-				<div style={{ display: 'flex', alignItems: 'center', marginBottom: 25 }}>
+				<div
+					style={{ display: 'flex', alignItems: 'center', marginBottom: 25 }}>
 					<Space style={{ flexGrow: 1 }}>
 						<Avatar
 							src={
 								<Link to={`/${user?.username}`}>
-									<Image crossOrigin='anonymous' src={user?.profile.profile_pic.url} preview={false} />
+									<Image
+										crossOrigin='anonymous'
+										src={user?.profile.profile_pic.url}
+										preview={false}
+									/>
 								</Link>
 							}
 							style={{ width: 60, height: 60, border: 'thin solid #9e9e9e' }}
@@ -68,7 +87,10 @@ const Sider: FC<SiderProps> = ({ fetchData }) => {
 								alignItems: 'start',
 								gap: 3,
 							}}>
-							<Typography.Text title={user?.username} strong style={{ fontSize: '1.125em' }}>
+							<Typography.Text
+								title={user?.username}
+								strong
+								style={{ fontSize: '1.125em' }}>
 								<Link to={`/${user?.username}`} style={{ color: 'inherit' }}>
 									{user?.username}
 								</Link>
@@ -89,12 +111,20 @@ const Sider: FC<SiderProps> = ({ fetchData }) => {
 					renderItem={bot => (
 						<List.Item
 							actions={[
-								<Button type='primary' size='small' onClick={() => handleFollowRequest(bot.id)}>
+								<Button
+									type='primary'
+									size='small'
+									onClick={() => handleFollowRequest(bot.id)}>
 									follow
 								</Button>,
 							]}>
 							<List.Item.Meta
-								avatar={<Avatar crossOrigin='anonymous' src={bot.profile.profile_pic.url} />}
+								avatar={
+									<Avatar
+										crossOrigin='anonymous'
+										src={bot.profile.profile_pic.url}
+									/>
+								}
 								title={<Link to={`/${bot.username}`}>{bot.username}</Link>}
 								description={bot.profile.full_name}
 							/>
@@ -112,7 +142,10 @@ const Sider: FC<SiderProps> = ({ fetchData }) => {
 				}}>
 				<Typography.Text type='secondary'>
 					Designed and Developed by&nbsp;
-					<Typography.Link href='https://github.com/Yellow-May' target='_blank' rel='noreferrer'>
+					<Typography.Link
+						href='https://github.com/Yellow-May'
+						target='_blank'
+						rel='noreferrer'>
 						Yellow May
 					</Typography.Link>
 				</Typography.Text>
